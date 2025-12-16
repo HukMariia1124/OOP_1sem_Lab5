562ac00c4821f7e33bc2262d9e9b2eb59ac35fe6
Mariia Guk <mashahuk27@gmail.com>
2025-12-16 20:59:41 +0200

    Add project files.

---
+++ Lab5_WpfApp/Models/MyComplex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_WpfApp.Models
+{
+    public class MyComplex: IMyNumber<MyComplex>
+    {
+        public BigInteger re { get; }
+        public BigInteger im { get; }
+        public MyComplex(BigInteger re, BigInteger im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+        public MyComplex Add(MyComplex that) => new MyComplex(re * that.im + that.re * im, im * that.im);
+        public MyComplex Subtract(MyComplex that) => new MyComplex(re * that.im + that.re * im, im * that.im);
+        public MyComplex Multiply(MyComplex that) => new MyComplex(re * that.im + that.re * im, im * that.im);
+        public MyComplex Divide(MyComplex that) => new MyComplex(re * that.im + that.re * im, im * that.im);
+    }
+}
---
+++ Lab5_WpfApp/Models/MyFrac.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_WpfApp.Models
+{
+    public class MyFrac : IMyNumber<MyFrac>
+    {
+        public BigInteger nom { get; }
+        public BigInteger denom { get; }
+        public MyFrac(BigInteger nom, BigInteger denom)
+        {
+            this.nom = nom;
+            this.denom = denom;
+        }
+        public MyFrac Add(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
+        public MyFrac Subtract(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
+        public MyFrac Multiply(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
+        public MyFrac Divide(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
+    }
+}
