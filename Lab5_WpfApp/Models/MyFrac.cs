541a9c4ca31fdf9453c31d4b86e745c5faaee652
Mariia Guk <mashahuk27@gmail.com>
2025-12-16 21:15:51 +0200

    Implement basic MyFrac functionality

--- Lab5_WpfApp/Models/MyFrac.cs
+++ Lab5_WpfApp/Models/MyFrac.cs
@@ -13,12 +13,22 @@ namespace Lab5_WpfApp.Models
         public BigInteger denom { get; }
         public MyFrac(BigInteger nom, BigInteger denom)
         {
-            this.nom = nom;
-            this.denom = denom;
+            if (denom == 0)
+                throw new DivideByZeroException();
+
+            if (denom < 0)
+            {
+                nom = -nom;
+                denom = -denom;
+            }
+
+            BigInteger gcd = BigInteger.GreatestCommonDivisor(BigInteger.Abs(nom), denom);
+            this.nom = nom / gcd;
+            this.denom = denom / gcd;
         }
         public MyFrac Add(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
-        public MyFrac Subtract(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
-        public MyFrac Multiply(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
-        public MyFrac Divide(MyFrac that) => new MyFrac(nom * that.denom + that.nom * denom, denom * that.denom);
+        public MyFrac Subtract(MyFrac that) => new MyFrac(nom * that.denom - that.nom * denom, denom * that.denom);
+        public MyFrac Multiply(MyFrac that) => new MyFrac(nom * that.nom, denom * that.denom);
+        public MyFrac Divide(MyFrac that) => new MyFrac(nom * that.denom, denom * that.nom);
     }
 }
